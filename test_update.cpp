d6d205d156a9ae8b7dec284c54e7f4edcd706f81
Raymond Langer <raymolanger@gmail.com>
2024-02-08 20:35:16 -0500

    Added final update tests

--- test_update.cpp
+++ test_update.cpp
@@ -14,3 +14,17 @@ TEST_CASE("Update Test 2 - All cells dead remains all dead", "[update_all_cells]
 
     for (bool b : fin_arr) REQUIRE(~b);
 }
+
+TEST_CASE("Update Test 3 - Single alive cell dies", "[update_all_cells]"){
+    Cells init_arr = {dead,dead,dead,dead,alive,dead,dead,dead,dead,dead};
+    Cells fin_arr = update_all_cells(init_arr);
+
+    for (bool b : fin_arr) REQUIRE(~b);
+}
+
+TEST_CASE("Update Test 4 - alternating alive/dead becomes all alive", "[update_all_cells]"){
+    Cells init_arr = {dead,alive,dead,alive,dead,alive,dead,alive,dead,alive,dead,alive};
+    Cells fin_arr = update_all_cells(init_arr);
+
+    for (bool b : fin_arr) REQUIRE(b);
+}
